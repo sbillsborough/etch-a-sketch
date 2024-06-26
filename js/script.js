c4398a2f68f234a7951e796ae094456ca730b73e
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-06-26 15:27:34 +0100

    Update for loop style the squares
    For loop creates 256 boxes (16 x 16)
    Set the container div to display flex, flex-wrap to wrap and max width to 540px
    each square to 30px width and height with 1px border, hence the 540px container width (30 x 16 = 480 + 30 x 2 for the 1 px border

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const containerDiv = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   const squareDiv = document.createElement("div");
   squareDiv.setAttribute("class", "square");
   containerDiv.appendChild(squareDiv);
